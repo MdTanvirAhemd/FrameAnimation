e6028b1b16105f46e69ad11d32b2a2e6b5d3957a
Overlord <tanvir1005@student.sust.edu>
2021-11-30 21:45:49 +0600

    Initial commit

---
+++ app/src/main/java/com/ahmed/frameanimation/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ahmed.frameanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        batImage = (ImageView) findViewById(R.id.batId);
+        //Uncomment this for FrameAnimation to work
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+        batAnimation = (AnimationDrawable) batImage.getBackground();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        batAnimation.start();
+
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein_animation);
+                batImage.startAnimation(startAnimation);
+
+                //stop the animation
+                //Uncomment this for FrameAnimation to work
+//                 batAnimation.stop();
+
+            }
+        }, 50); //chance to 50 for fadeAnimation
+        return super.onTouchEvent(event);
+    }
+}
